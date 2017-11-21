a9ebae1941bfb7d4464560c3b503c583e43f4c54
Buingoc <ngoctbhy@gmail.com>
2017-11-22 00:57:40 +0700

    ADD

--- public/js/socket/client.js
+++ public/js/socket/client.js
@@ -243,3 +243,24 @@ function textAreaAdjust(o) {
     // }
     // console.log($('.scrollbar').height(), $('.scrollbar').innerHeight(), $('.scrollbar').outerHeight())
 }
+
+function FitToContent(id, maxHeight) {
+    var text = id && id.style ? id : document.getElementById(id);
+    if (!text)
+        return;
+
+    var adjustedHeight = text.clientHeight;
+    if (!maxHeight || maxHeight > adjustedHeight) {
+        adjustedHeight = Math.max(text.scrollHeight, adjustedHeight);
+        if (maxHeight)
+            adjustedHeight = Math.min(maxHeight, adjustedHeight);
+        if (adjustedHeight > text.clientHeight)
+            text.style.height = adjustedHeight + "px";
+    }
+}
+
+window.onload = function () {
+    document.getElementById("boxChat").onkeyup = function () {
+        FitToContent(this, 130)
+    };
+}
